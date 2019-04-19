a9f078805a63eac9a645b2090a9faedcd3a5545d
waby <wabyking@163.com>
2019-04-19 21:01:02 +0200

    ~

---
+++ models/__init__.py
@@ -0,0 +1,27 @@
+
+# -*- coding: utf-8 -*-
+
+from models.LSTM1 import LSTM1
+from models.CNN import CNN
+from models.BiLSTM import BiLSTM
+from models.TwoLayerLSTM import TwoLayerLSTM
+
+
+
+def setup(opt):
+    
+    if opt.contatenate==1:
+            opt.max_sequence_length = opt.max_sequence_length_contatenate  
+            
+    if opt.model == "lstm":
+        model = LSTM1(opt)
+    elif opt.model == "cnn":
+        model = CNN(opt)
+    elif opt.model == "bilstm":
+        model = BiLSTM(opt)
+    elif opt.model == "bilstm2":
+        model = TwoLayerLSTM(opt)
+    else:
+        raise Exception("model not supported: {}".format(opt.model))
+
+    return model
